39c647c6b55bca41ebd04c169478055a2fb8220f
Mohd Shariq <mshariq@sugarcrm.com>
2016-08-26 14:03:05 +0530

    refector SafariBrowserInterface class

--- src/main/java/com/sugarcrm/candybean/automation/webdriver/SafariBrowserInterface.java
+++ src/main/java/com/sugarcrm/candybean/automation/webdriver/SafariBrowserInterface.java
@@ -2,10 +2,14 @@ package com.sugarcrm.candybean.automation.webdriver;
 
 import java.io.File;
 import java.io.IOException;
+
 import com.sugarcrm.candybean.utilities.OSValidator;
+
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.StringUtils;
+
 import com.sugarcrm.candybean.exceptions.CandybeanException;
+
 import org.openqa.selenium.safari.SafariDriver;
 import org.openqa.selenium.support.ThreadGuard;
 
@@ -18,28 +22,28 @@ public class SafariBrowserInterface extends WebDriverInterface {
 	@Override
 	public void start() throws CandybeanException {
 		String safariDriverPath = candybean.config.getPathValue("browser.safari.driver.path");
-		validateChromeDriverExist(safariDriverPath);
+		validateSafariDriverExist(safariDriverPath);
 
 		// If parallel is enabled and the safaridriver-<os>_<thread-name> doesn't exist, duplicate one
 		// from safari-<os> and give it executable permission.
-		if("true".equals(candybean.config.getPathValue("parallel.enabled"))) {
-			String originalChromePath = safariDriverPath;
+		if ("true".equals(candybean.config.getPathValue("parallel.enabled"))) {
+			String originalSafariPath = safariDriverPath;
 			// Cross platform support
-			if(OSValidator.isWindows()) {
+			if (OSValidator.isWindows()) {
 				safariDriverPath = safariDriverPath.replaceAll("(.*)(\\.exe)", "$1_" +
 						Thread.currentThread().getName() + "$2");
 			} else {
 				safariDriverPath = safariDriverPath.replaceAll("$", "_" + Thread.currentThread().getName());
 			}
 
-			if(!new File(safariDriverPath).exists()) {
+			if (!new File(safariDriverPath).exists()) {
 				try {
-					FileUtils.copyFile(new File(originalChromePath), new File(safariDriverPath));
-					if(!OSValidator.isWindows()) { //Not needed in Windows
+					FileUtils.copyFile(new File(originalSafariPath), new File(safariDriverPath));
+					if (!OSValidator.isWindows()) { //Not needed in Windows
 						Runtime.getRuntime().exec("chmod u+x " + safariDriverPath);
 					}
-				} catch(IOException e) {
-					String error = "Cannot duplicate a new chromedriver for parallelization";
+				} catch (IOException e) {
+					String error = "Cannot duplicate a new safaridriver for parallelization";
 					logger.severe(error);
 					throw new CandybeanException(error);
 				}
@@ -48,15 +52,15 @@ public class SafariBrowserInterface extends WebDriverInterface {
 
 		logger.info("safariDriverPath: " + safariDriverPath);
 		System.setProperty("webdriver.safari.driver", safariDriverPath);
-		logger.info("Instantiating Chrome with:\n    driver path: " + safariDriverPath);
+		logger.info("Instantiating Safari with:\n    driver path: " + safariDriverPath);
 
 		super.wd = ThreadGuard.protect(new SafariDriver());
 		super.start(); // requires wd to be instantiated first
 	}
 
-	private void validateChromeDriverExist(String safariDriverPath) throws CandybeanException {
-		if(StringUtils.isEmpty(safariDriverPath) || !new File(safariDriverPath).exists()) {
-			String error = "Unable to find safari browser driver from the specified location ("+safariDriverPath+") " +
+	private void validateSafariDriverExist(String safariDriverPath) throws CandybeanException {
+		if (StringUtils.isEmpty(safariDriverPath) || !new File(safariDriverPath).exists()) {
+			String error = "Unable to find safari browser driver from the specified location (" + safariDriverPath + ") " +
 					"in the configuration file! \n Please add a configuration to the candybean config file for key \"browser.safari.driver.path\" "
 					+ "that indicates the absolute or relative location the driver.";
 			logger.severe(error);
